6ae2b0d8e891b7c27df9aa485243fa43331a85b4
Th1nkK1D <witheep@gmail.com>
2022-09-21 11:29:57 +0300

    fix: Make adjust counter become unclickable

--- src/components/LandingComponent/AdjustCount.svelte
+++ src/components/LandingComponent/AdjustCount.svelte
@@ -2,10 +2,8 @@
 	export let text;
 </script>
 
-<button>
-	<p
-		class="font-anakotmai text-[32px] md:text-6xl tracking-[0.24em] counting_number_big text-white"
-	>
-		{text}
-	</p>
-</button>
+<p
+	class="font-anakotmai text-[32px] md:text-6xl tracking-[0.24em] counting_number_big text-white"
+>
+	{text}
+</p>
