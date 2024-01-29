29cda1d1849bcdfdf36f9cbcc7b9c3b15db639bf
Th1nkK1D <witheep@gmail.com>
2024-01-29 18:52:29 +0700

    feat(web): update firebase config env

--- web/src/config/firebase.ts
+++ web/src/config/firebase.ts
@@ -1,11 +1,12 @@
-import { initializeApp, getApps } from "firebase/app";
-import firebaseConfig from './firebaseConfig'
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 export function getFirebase() {
-    if (getApps().length == 0) {
-        initializeApp(firebaseConfig)
-    }
+	if (getApps().length == 0) {
+		initializeApp(
+			JSON.parse((import.meta.env.VITE_FIREBASE_CONFIG as string) ?? '{}')
+		);
+	}
 
-    return { db: getFirestore() }
+	return { db: getFirestore() };
 }
--- web/src/config/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
-
-export default firebaseConfig;
